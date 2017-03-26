2058e37e79c88a1a53d006eb2e7a0e622a1d95c2
rajashravan <rajashravan@hotmail.com>
2017-03-26 14:50:54 -0400

    meta files, assets and scripts

---
+++ 2D Roguelike/Assets/Scripts/GameManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public static GameManager instance = null;
+    //static means variable will belong to class itself, rather than an instance of the class
+    public BoardManager boardScript;
+
+    private int level = 3;
+
+	// Use this for initialization
+	void Awake () {
+        if (instance == null) instance = this;
+        else if (instance != this) Destroy(gameObject);
+        DontDestroyOnLoad(gameObject);
+        boardScript = GetComponent<BoardManager>();
+        InitGame();
+		
+	}
+
+    void InitGame()
+    {
+        boardScript.SetupScene(level);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ 2D Roguelike/Assets/Scripts/MovingObject.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class MovingObject : MonoBehaviour {
+
+    public float moveTime = 0.1f;
+    public LayerMask blockingLayer;
+
+    private BoxCollider2D boxCollider;
+    private Rigidbody2D rb2D;
+    private float inverseMoveTime;
+
+	// Use this for initialization
+	protected virtual void Start () {
+        boxCollider = GetComponent<BoxCollider2D>;
+        rb2D = GetComponent<Rigidbody2D>();
+        inverseMoveTime = 1f / moveTime;
+
+		
+	}
+
+    protected IEnumerator SmoothMovement (Vector3 end)
+    {
+        float sqrRemainingDistance = (transform.position - end).sqrMagnitude;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
